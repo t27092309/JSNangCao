dd7b150c33c21bd2445b09ad7ba5deebc4823c29
t27092309 <truongdoxuan796@gmail.com>
2024-10-22 09:55:14 +0700

    validate

--- Source/DeThiThu/main.js
+++ Source/DeThiThu/main.js
@@ -1,420 +1,158 @@
-<<<<<<< HEAD
-
-import { getAllProduct,deleteProduct,addProduct,getProductById,updateProduct} from "./services.js";
-
-const app = {
-    // key : value
-    // hiển thị giao diện tbale
-    renderProductList :async function(){
-        // 1. call api lấy danh sách sản phẩm
-        const data = await getAllProduct()
-
-        // console.log(data);
-        
-        // 2. duyệt mảng data
-        const trList = data?.map((item,index)=>{
-            return `
+import { getAllProduct, getProductById, deleteProduct, addProduct, updateProduct } from "./services.js";
+const app={
+    renderProductList: async function(){
+        const data = await getAllProduct();
+        const proList = data?.map((item,index)=>{
+            return`
                 <tr>
-                    <th scope="row">${index +1 }</th>
+                    <th scope="row">${index+1}</th>
                     <td>${item.name}</td>
                     <td>${item.quantity}</td>
-                    <td><img style="height:70px" src="${item.image}" alt=""></td>
+                    <td><img src="${item.image}" style="width: 100px;"></td>
                     <td>
-                        <button data-id="${item.id}" class="btn_edit btn btn-warning">Sửa</button>
-                        <button data-id="${item.id}" class="btn_delete btn btn-danger">Xóa</button>
+                        <button data-id="${item.id}" class="btn_edit btn btn-warning">Edit</button>
+                        <button data-id="${item.id}" class="btn_delete btn btn-danger">Delete</button>
                     </td>
                 </tr>
             `
-        }).join("")// trả về 1 chuỗi
-        // console.log(trList);
-        
-        //3. đổ trList ra tbody
-
+        }).join("");
         const tbodyElement = document.querySelector('tbody');
-        tbodyElement.innerHTML = trList
-
-        // Logic xóa sản phẩm
-        this.handleDelete();
-        // xử lý cập nhật sản phẩm
+        tbodyElement.innerHTML = proList;
         this.handleEditProduct();
-        
+        this.handleDeleteProduct();
     },
-    handleDelete: function(){
-        //1.Tạo giao diện
-        // 2. lấy toàn bộ nút xóa
-        const btnDeletes = document.querySelectorAll('.btn_delete');
-        // console.log(btnDeletes);
-        // 3. định nghĩa sự kiện cho từng nút xóa
-        btnDeletes.forEach((item)=>{
+    handleDeleteProduct: function(){
+        const btnDelete = document.querySelectorAll('.btn_delete');
+        btnDelete.forEach((item)=>{
             item.addEventListener('click',()=>{
-                // console.log(item);
-                // 4. lấy id của phần tử cần xóa
-                // cách 1:
-                // const id = item.dataset.id;
-                //cách 2:
-                const id = item.getAttribute("data-id")
-                // console.log(id);
-                // 5. Xóa
-                if(window.confirm("Bạn có chắc chắn muốn xóa không?")){
-                    deleteProduct(id)
+                const id = item.getAttribute("data-id");
+                if(window.confirm('Delete???')){
+                    deleteProduct(id);
                 }
-                
             })
         })
-        
     },
     renderAddProduct: function(){
-        // địng nghĩa sự kiện click nút add
         const btnAdd = document.getElementById('btn_add');
         btnAdd.addEventListener('click',()=>{
-            // console.log("add!!!");
             const content = document.getElementById('content');
-            // thay thế nội dung thêm mới (form)
-            content.innerHTML= `
-                <h1> Thêm mới sản phẩm</h1>
-                <form id="form">
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Tên sản phẩm</label>
-                        <input type="text" class="form-control" id="name">
-                    </div>
+            content.innerHTML=`
+                    <form action="" id="form">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="quantity" class="form-label">Quantity</label>
+                            <input type="text" class="form-control" id="quantity">
+                        </div>
+                        <div class="mb-3">
+                            <label for="image" class="form-label">Image</label>
+                            <input type="text" class="form-control" id="image">
+                        </div>
 
-                    <div class="mb-3">
-                        <label for="quantity" class="form-label">Số lượng sản phẩm</label>
-                        <input type="number" class="form-control" id="quantity">
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="image" class="form-label">Hình ảnh</label>
-                        <input type="text" class="form-control" id="image">
-                    </div>
-                    
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
             `
-            
             const form = document.getElementById('form');
             form.addEventListener('submit',(event)=>{
-                // ngăn chặn hành vi mặc định (tải trang)
                 event.preventDefault();
-                // xử lý logic thêm mới sản phẩm
                 this.handleAddProduct();
-                
             })
         })
     },
     handleAddProduct: async function(){
-        //lấy ra toàn bộ thẻ input
-        const inputName = document.getElementById('name')
-        const inputQuantity = document.getElementById('quantity')
-        const inputImage = document.getElementById('image')
+        const inpName = document.getElementById('name')
+        const inpQuantity = document.getElementById('quantity')
+        const inpImage = document.getElementById('image')
 
-        // validate
-
-        if(!inputName.value.trim()){
-            alert("Cần nhập thông tin tên sản phẩm");
-            inputName.focus(); // focus vào ô input đang bị lỗi
-            return; // ngăn chặn thực thi các tác vụ tiếp theo
-        }
-
-        if(!inputQuantity.value.trim()){
-            alert("Cần nhập thông tin số lượng sản phẩm");
-            inputQuantity.focus(); // focus vào ô input đang bị lỗi
+        if(!inpName.value.trim()){
+            alert('Cần nhập thông tin tên sản phẩm');
+            inpName.focus();
             return;
         }
-
-        if(!inputImage.value.trim()){
-            alert("Cần nhập thông tin hình ảnh sản phẩm");
-            inputImage.focus(); // focus vào ô input đang bị lỗi
+        if(!inpQuantity.value.trim()){
+            alert('Cần nhập thông tin số lượng sản phẩm');
+            inpQuantity.focus();
             return;
         }
-
-        // lấy dữ liệu
-        const data = { // KHÔNG cần thêm id vì json-server sẽ tự động tạo id khi thêm mới
-            name: inputName.value,
-            quantity: Number(inputQuantity.value),
-            image: inputImage.value
+        if(!inpImage.value.trim()){
+            alert('Cần nhập thông tin hình ảnh sản phẩm');
+            inpImage.focus();
+            return;
         }
-
-        console.log(data);
-        // thêm data vào db.json
+        const data = {
+            name: inpName.value,
+            quantity: Number(inpQuantity.value),
+            image: inpImage.value
+        }
         await addProduct(data);
-        alert("Thêm thành công")
-
+        alert("Add successfully");
     },
     handleEditProduct: function(){
-        // lấy toàn bộ nút bấm
-        const btnEdits = document.querySelectorAll('.btn_edit');
-        btnEdits.forEach((item)=>{
-            item.addEventListener('click',async ()=>{
-                // console.log(item);
-                // lấy id 
+        const btnEdit = document.querySelectorAll('.btn_edit');
+        btnEdit.forEach((item)=>{
+            item.addEventListener('click', async()=>{
                 const id = item.dataset.id;
-                // console.log(id);
-                // lấy thông tin sản phẩm theo id
                 const product = await getProductById(id);
-                // console.log(product);
-
-                // thay thể table -> form (cập nhật)
                 const content = document.getElementById('content');
-                // thay thế nội dung thêm mới (form)
-                content.innerHTML= `
-                    <h1> Cập nhật sản phẩm</h1>
-                    <form id="form">
+                content.innerHTML = `
+                    <form action="" id="form">
                         <div class="mb-3">
-                            <label for="name" class="form-label">Tên sản phẩm</label>
+                            <label for="name" class="form-label">Name</label>
                             <input type="text" class="form-control" id="name" value="${product.name}">
                         </div>
-
                         <div class="mb-3">
-                            <label for="quantity" class="form-label">Số lượng sản phẩm</label>
-                            <input type="number" class="form-control" id="quantity" value="${product.quantity}">
+                            <label for="quantity" class="form-label">Quantity</label>
+                            <input type="text" class="form-control" id="quantity" value="${product.quantity}">
                         </div>
-
                         <div class="mb-3">
-                            <label for="image" class="form-label">Hình ảnh</label>
+                            <label for="image" class="form-label">Image</label>
                             <input type="text" class="form-control" id="image" value="${product.image}">
                         </div>
-                        
-                        <button type="submit" class="btn btn-primary">Submit</button>
+
+                        <button type="submit" class="btn btn-primary">Update</button>
                     </form>
                 `
                 const form = document.getElementById('form');
                 form.addEventListener('submit',(event)=>{
-                    // ngăn chặn hành vi mặc định (tải trang)
                     event.preventDefault();
-                    // xử lý logic cập nhật sản phẩm
                     this.handleUpdateProduct(id);
-                    
                 })
-                
             })
         })
     },
-    handleUpdateProduct:async function(id){
-        //lấy ra toàn bộ thẻ input
-        const inputName = document.getElementById('name')
-        const inputQuantity = document.getElementById('quantity')
-        const inputImage = document.getElementById('image')
+    handleUpdateProduct: async function(id){
+        const inpName = document.getElementById('name')
+        const inpQuantity = document.getElementById('quantity')
+        const inpImage = document.getElementById('image')
 
-        // validate
-
-        if(!inputName.value.trim()){
-            alert("Cần nhập thông tin tên sản phẩm");
-            inputName.focus(); // focus vào ô input đang bị lỗi
-            return; // ngăn chặn thực thi các tác vụ tiếp theo
-        }
-
-        if(!inputQuantity.value.trim()){
-            alert("Cần nhập thông tin số lượng sản phẩm");
-            inputQuantity.focus(); // focus vào ô input đang bị lỗi
+        if(!inpName.value.trim()){
+            alert('Cần nhập thông tin tên sản phẩm');
+            inpName.focus();
             return;
         }
-
-        if(!inputImage.value.trim()){
-            alert("Cần nhập thông tin hình ảnh sản phẩm");
-            inputImage.focus(); // focus vào ô input đang bị lỗi
+        if(!inpQuantity.value.trim()){
+            alert('Cần nhập thông tin số lượng sản phẩm');
+            inpQuantity.focus();
             return;
         }
-
-        // lấy dữ liệu
-        const data = { // KHÔNG cần thêm id vì json-server sẽ tự động tạo id khi thêm mới
-            name: inputName.value,
-            quantity: Number(inputQuantity.value),
-            image: inputImage.value
+        if(!inpImage.value.trim()){
+            alert('Cần nhập thông tin hình ảnh sản phẩm');
+            inpImage.focus();
+            return;
         }
-
-        console.log(data);
-        // cập nhật data vào db.json
-        await updateProduct(id,data)
-        alert("cập nhật thành công")
+        const data = {
+            name: inpName.value,
+            quantity: Number(inpQuantity.value),
+            image: inpImage.value
+        }
+        await updateProduct(id,data);
+        alert("Update successfully");
     },
     start: function(){
-        // console.log(123);
-        // render : Hiển thị ra giao diện
-        // handle : Xử lý logic cho chức năng
         this.renderProductList();
         this.renderAddProduct();
     }
 }
-
-=======
-// get: danh sách, object
-// post: thêm mới
-// put: cập nhật
-// delete: xóa
-
-import { getAllProduct, deleteProduct, addProduct, getProductById, updateProduct } from "./services.js";
-
-const app = {
-	// key - value
-	// hiển thị giao diện danh sách
-	renderProductList: async function () {
-		// 1. Lấy dữ liệu từ db.json (user)
-		const data = await getAllProduct();
-		// 2. duyệt mảng data và đổ dữ liệu ra
-
-		// 2.1. duyệt mảng và đổ vào tr
-		const proList = data?.map((item, index) => {
-			return `
-				<tr>
-           		 	<th scope="row">${index + 1}</th>
-          			<td>${item.name}</td>
-          			<td>${item.quantity}</td>
-          			<td style="max-width: 100px;"><img style="max-width: 100px;" src="${item.image}" alt=""></td>
-          			<td>
-						<button data-id ="${item.id}" class="btn_edit btn btn-warning">Edit</button>
-						<button data-id ="${item.id}" class="btn_delete btn btn-danger">Delete</button>
-					</td>
-				</tr>
-            `
-		}).join(''); // chuyển 1 mảng về 1 chuỗi
-		const tbodyElement = document.querySelector('tbody');
-		tbodyElement.innerHTML = proList;
-
-		this.handleDelete();
-		this.handleEditProduct();
-	},
-	handleDelete: function () {
-		const btnDelete = document.querySelectorAll('.btn_delete');
-		btnDelete.forEach((item) => {
-			item.addEventListener('click', () => {
-				const id = item.getAttribute("data-id");
-				if (window.confirm("Delete???")) {
-					deleteProduct(id)
-				}
-			})
-		})
-	},
-	renderAddProduct: function () {
-		const btnAdd = document.getElementById('btn_add');
-		btnAdd.addEventListener('click', () => {
-			const content = document.getElementById('content');
-			content.innerHTML = `
-				    <form action="" id="form">
-				<div class="mb-3">
-					<label for="name" class="form-label">Tên sản phẩm</label>
-					<input type="text" class="form-control" id="name">
-				</div>
-
-				<div class="mb-3">
-					<label for="quantity" class="form-label">Số lượng sản phẩm</label>
-					<input type="text" class="form-control" id="quantity">
-				</div>
-
-				<div class="mb-3">
-					<label for="img" class="form-label">Hình ảnh</label>
-					<input type="text" class="form-control" id="img">
-				</div>
-
-				<button type="submit" class="btn btn-primary">Submit</button>
-				</form>
-			`
-			const form = document.getElementById('form');
-			form.addEventListener('submit', (event) => {
-				event.preventDefault();
-				this.handleAddProduct();
-			})
-		})
-	},
-	handleAddProduct: async function () {
-		const inputName = document.getElementById('name');
-		const inputQuantity = document.getElementById('quantity');
-		const inputImg = document.getElementById('img');
-
-		if (!inputName.value.trim()) {
-			alert("Cần nhập tên sản phẩm");
-			inputName.focus();
-			return;
-		}
-		if (!inputQuantity.value.trim()) {
-			alert("Cần nhập thông tin số lượng sản phẩm");
-			inputQuantity.focus();
-			return;
-		}
-		if (!inputImg.value.trim()) {
-			alert("Cần nhập thông tin hình ảnh sản phẩm");
-			inputImg.focus();
-			return;
-		}
-
-		const data = {
-			name: inputName.value,
-			quantity: Number(inputQuantity.value),
-			image: inputImg.value
-		}
-		await addProduct(data);
-		alert("Add successfully");
-	},
-	handleEditProduct: function () {
-		const btnEdit = document.querySelectorAll('.btn_edit');
-		btnEdit.forEach((item) => {
-			item.addEventListener('click', async () => {
-				const id = item.dataset.id;
-				const product = await getProductById(id);
-				const content = document.getElementById('content');
-
-				content.innerHTML = `
-					<form action="" id="form">
-					<div class="mb-3">
-						<label for="name" class="form-label">Tên sản phẩm</label>
-						<input type="text" class="form-control" id="name" value="${product.name}">
-					</div>
-
-					<div class="mb-3">
-						<label for="quantity" class="form-label">Số lượng sản phẩm</label>
-						<input type="text" class="form-control" id="quantity" value="${product.quantity}">
-					</div>
-
-					<div class="mb-3">
-						<label for="img" class="form-label">Hình ảnh</label>
-						<input type="text" class="form-control" id="img" value="${product.img}">
-					</div>
-
-					<button type="submit" class="btn btn-primary">Submit</button>
-					</form>
-				`
-				const form = document.getElementById('form');
-				form.addEventListener('submit', (event) => {
-					event.preventDefault();
-					this.handleUpdateProduct(id);
-				})
-			})
-		})
-	},
-	handleUpdateProduct: async function(id){
-		const inputName = document.getElementById('name')
-		const inputQuantity = document.getElementById('quantity')
-		const inputImg = document.getElementById("img")
-
-		if (!inputName.value.trim()) {
-			alert("Cần nhập tên sản phẩm");
-			inputName.focus();
-			return;
-		}
-		if (!inputQuantity.value.trim()) {
-			alert("Cần nhập thông tin số lượng sản phẩm");
-			inputQuantity.focus();
-			return;
-		}
-		if (!inputImg.value.trim()) {
-			alert("Cần nhập thông tin hình ảnh sản phẩm");
-			inputImg.focus();
-			return;
-		}
-
-		const data = {
-			name: inputName.value,
-			quantity: Number(inputQuantity.value),
-			image: inputImg.value
-		}
-		await updateProduct(id,data);
-		alert("Update successfully");
-	},
-start: function(){
-	this.renderProductList();
-	this.renderAddProduct();
-}
-
-}
->>>>>>> 621a0a6af7d14df10ef1a71aa67a09fc194e206a
 app.start();
--- Source/DeThiThu/services.js
+++ Source/DeThiThu/services.js
@@ -1,76 +1,26 @@
-// get: lấy danh sách, bản ghi
-// post: thêm mới
-<<<<<<< HEAD
-// put: sửa
-// delete: xóa
-
-export const getAllProduct =async ()=>{
-    try {
-        const res = await fetch(`http://localhost:3000/products`);// mặc định = get
-=======
-// put: sửa 
-// delete: xóa
-
-export const getAllProduct = async ()=>{
+export const getAllProduct = async()=>{
     try{
         const res = await fetch(`http://localhost:3000/products`);
->>>>>>> 621a0a6af7d14df10ef1a71aa67a09fc194e206a
-        // console.log(res);
         const data = await res.json();
-        // console.log(data);
-        return data
-<<<<<<< HEAD
-    } catch (error) {
-        alert("Lỗi")
-    }
-}
-
-export const deleteProduct = async (id)=>{
-    try {
-        await fetch(`http://localhost:3000/products/${id}`,{
-            method:"delete"
-        })
-        alert("Xóa thành công")
-
-    } catch (error) {
-        alert("Xóa thất bại")
-    }
-}
-
-export const addProduct = async (data)=>{
-    try{
-        await fetch(`http://localhost:3000/products`,{
-            method: "post",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)// chuyển dữ liệu từ object -> JSON
-        })
-
-    }catch(err){
-        alert("Lỗi")
-    }
-}
-
-export const getProductById =async(id)=>{
-    try {
-        const res  = await fetch(`http://localhost:3000/products/${id}`)
-        const data = await res.json();
-
         return data;
-    } catch (error) {
-        alert("Lỗi")
-    }
-
-=======
     }catch(error){
         alert("Error")
     }
 }
 
+export const getProductById = async(id)=>{
+    try{
+        const res = await fetch(`http://localhost:3000/products/${id}`);
+        const data = await res.json();
+        return data;
+    }catch(error){
+        alert("Get Product Error")
+    }
+}
+
 export const deleteProduct = async(id)=>{
     try{
-        await fetch(`http://localhost:3000/product/${id}`,{
+        await fetch(`http://localhost:3000/products/${id}`,{
             method: "delete"
         })
         alert("Delete successfully")
@@ -81,53 +31,28 @@ export const deleteProduct = async(id)=>{
 
 export const addProduct = async(data)=>{
     try{
-        await fetch(`http://localhost:3000/product`,{
+        await fetch(`http://localhost:3000/products`,{
             method: "post",
-            header: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-    }catch(error){
-        alert("Error")
-    }
-}
-
-export const getProductById = async(id)=>{
-    try{
-        const res = await fetch(`http://localhost:3000/product/${id}`)
-        const data = await res.json();
-
-        return data;
-    }catch(error){
-        alert("Error")
-    }
->>>>>>> 621a0a6af7d14df10ef1a71aa67a09fc194e206a
-}
-
-export const updateProduct = async (id,data)=>{
-    try{
-<<<<<<< HEAD
-        await fetch(`http://localhost:3000/products/${id}`,{
-            method: "put", // cập nhật
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data)// chuyển dữ liệu từ object -> JSON
+            body: JSON.stringify(data)
         })
+    }catch(error){
+        alert("Add failed")
+    }
+}
 
-    }catch(err){
-        alert("Lỗi")
-=======
-        await fetch(`http://localhost:3000/product/${id}`,{
-            method: "put",
-            header: {
+export const updateProduct = async(id,data)=>{
+    try{
+        await fetch(`http://localhost:3000/products/${id}`,{
+            method: 'put',
+            headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         })
     }catch(error){
-        alert("Error")
->>>>>>> 621a0a6af7d14df10ef1a71aa67a09fc194e206a
+        alert("Update failed")
     }
 }
